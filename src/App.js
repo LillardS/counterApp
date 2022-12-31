0f77b2d216ed18263129214902d50d133ffb3e17
Seth Lillard <lillardseth@gmail.com>
2022-12-30 21:42:43 -0500

    basics done

--- src/App.js
+++ src/App.js
@@ -1,24 +1,21 @@
-import logo from './logo.svg';
-import './App.css';
+import React, {useState} from 'react';
 
 function App() {
+  const [count, setCount] = useState(0);
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    // <div className="container mt-5">
+      <div className='card text-center'>
+        <div className='card-body'>
+          <h1>How high can you count?</h1>
+          <div className='my-5'>
+            <h2 className='my-5 count'>{count}</h2>
+            <button className='btn mx-3 up' onClick={() => setCount(count + 1)}>Count Up</button>
+            <button className='btn mx-3 down' onClick={() => setCount(count - 1)}>Count Down</button>
+            <button className='btn mx-3 reset' onClick={() => setCount(0)}>Back to start</button>
+          </div>
+        </div>
+      </div>
+    // </div>
   );
 }
 
